94d3fc262565d060ecd48b4868912e6e54b37893
Rasmus Rudling <rasmusrudling@gmail.com>
2020-10-08 15:18:14 +0200

    Börjat med komponenten subsection

--- AI visualization/hmm-react-app/src/Components/Common/Subsection/subsection.js
+++ AI visualization/hmm-react-app/src/Components/Common/Subsection/subsection.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import classes from './subsection.module.css';
+import downArrow from '../../../Resources/down-arrow.svg';
 
 
 const Subsection = (props) => {
     return (
         <div className={classes.Subsection}>
-            <div className={classes.headerPart}>
-                <h2>{props.header}</h2>
+            <div className={classes.headerContainer}>
+                <div className={classes.headerPart}>
+                    <h2>{props.header}</h2>
+                </div>
 
-                <div className={classes.showHideContentToggler}>
-                
+                <div className={classes.showHideContentToggler} >
+                    <img src = {downArrow} alt="arrow" />
                 </div>
             </div>
+            
 
             <div className={classes.contentPart}>
                 {props.children}
--- AI visualization/hmm-react-app/src/Components/ForwardAlgorithm/forwardAlgorithm.js
+++ AI visualization/hmm-react-app/src/Components/ForwardAlgorithm/forwardAlgorithm.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import './forwardAlgorithm.module.css';
+import classes from './forwardAlgorithm.module.css';
 
 import Subsection from '../Common/Subsection/subsection'
 
 const ForwardAlgorithm = (props) => {
     return (
-        <div>
+        <div className={classes.ForwardAlgorithm}>
             <h1>Forward Algorithm</h1>
 
             <Subsection header = "About the Forward Algorithm">
-                <div>Hej</div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ligula elementum, malesuada magna sollicitudin, semper risus. Suspendisse vehicula, tortor non viverra placerat, arcu urna vestibulum mauris, ac tristique odio nunc in massa. In venenatis, eros sit amet fringilla euismod, sem magna efficitur sapien, eu sagittis odio erat et purus. Cras nec sapien fermentum, varius odio nec, ultricies mi. Suspendisse ut pretium nisi. Vestibulum dignissim orci eget nunc pharetra, ac ultrices risus posuere. Fusce placerat convallis congue. Integer quis efficitur enim, sit amet malesuada justo. Suspendisse sem magna, vehicula ac mattis quis, rhoncus in risus. Vivamus ac odio vitae urna congue scelerisque. Vivamus vulputate, lorem luctus vulputate dictum, enim nisl suscipit elit, eget tempus libero massa et sapien. Duis eleifend est diam, ut imperdiet tellus ullamcorper nec. Etiam venenatis orci massa, ut euismod elit pulvinar sed. Quisque vel ligula eros. Vestibulum et ultrices magna. Nullam tempor sed justo eget posuere.
+
+                    In nec nunc eget eros laoreet congue eget tristique sapien. Donec suscipit ipsum eget nunc egestas, ut egestas leo imperdiet. Nunc semper, ligula sit amet commodo egestas, tortor dolor vulputate ligula, eu semper mi ipsum ultrices justo. Nunc tincidunt sagittis dictum. Sed ut auctor nisl, at fermentum lacus. Nam vestibulum lectus ac eros sagittis, et volutpat massa tempor. Mauris in arcu justo. Phasellus in elit vitae nulla tincidunt molestie vel vel justo. Fusce eu sapien iaculis, condimentum velit at, hendrerit risus. Morbi dictum metus ligula, quis bibendum ligula pulvinar vitae. Pellentesque id magna neque. Cras vehicula lectus non nisl fringilla tempus id nec diam.
+
+                    Integer convallis lectus non sagittis facilisis. Fusce laoreet blandit suscipit. Sed accumsan finibus sapien, aliquam imperdiet dui pharetra quis. Aliquam a tortor neque. Ut accumsan posuere massa egestas rhoncus. Donec hendrerit nisi vel ante laoreet, eu tincidunt odio ornare. Mauris egestas ex ut diam sodales, et convallis sapien auctor. Vivamus ut tempus massa, ac sollicitudin leo. Duis nibh tellus, tristique sit amet mollis sit amet, tristique eu lacus. Curabitur rutrum suscipit pharetra. Nulla ultricies nisl a facilisis porta. Vestibulum venenatis metus lorem, non hendrerit felis egestas non.
+
+                </p>
             </Subsection>
         </div>
     );
