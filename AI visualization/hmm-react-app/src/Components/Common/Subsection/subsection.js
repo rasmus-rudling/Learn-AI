09196e3a0d75e053f3b9f9680003220955e6542b
Rasmus Rudling <rasmusrudling@gmail.com>
2020-10-08 20:23:25 +0200

    Klar med första delen på forward algorithm

--- AI visualization/hmm-react-app/src/Components/Common/Subsection/subsection.js
+++ AI visualization/hmm-react-app/src/Components/Common/Subsection/subsection.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import classes from './subsection.module.css';
 import downArrow from '../../../Resources/down-arrow.svg';
 
 
 const Subsection = (props) => {
+    const contentPartElement = useRef(null);
+    const arrowElement = useRef(null);
+    const arrowContainerElement = useRef(null);
+    const headerPartElement = useRef(null);
+    const [maxHeight, setMaxHeight] = useState(props.hideDefault ? "0px" : props.maxHeight);
+    const [paddingForContent, setPaddingForContent] = useState(props.hideDefault ? "0px" : "20px");
+
+    const toggleContent = () => {
+        arrowElement.current.classList.toggle(classes.rotateArrow);
+        arrowContainerElement.current.classList.toggle(classes.applyRadiusBottomRight);
+        headerPartElement.current.classList.toggle(classes.applyRadiusBottomLeft);
+        setMaxHeight(maxHeight == "0px" ? props.maxHeight : "0px");
+        setPaddingForContent(paddingForContent == "0px" ? "20px" : "0px");
+    }
+
+    let arrowClassList, headerPartClassList;
+
+    if (props.hideDefault) {
+        arrowClassList = [classes.showHideContentToggler, classes.applyRadiusBottomRight];
+        headerPartClassList = [classes.headerPart, classes.applyRadiusBottomLeft];
+    } else {
+        arrowClassList = [classes.showHideContentToggler];
+        headerPartClassList = [classes.headerPart];
+    }
+
+
+
     return (
         <div className={classes.Subsection}>
             <div className={classes.headerContainer}>
-                <div className={classes.headerPart}>
+                <div className={headerPartClassList.join(" ")} ref={headerPartElement}>
                     <h2>{props.header}</h2>
                 </div>
 
-                <div className={classes.showHideContentToggler} onClick = {() => {
-                    // Skapa ref till contentpart och skriv in : 
-                    //document.getElementById('slider').classList.toggle('closed');
-                }}>
-                    <img src = {downArrow} alt="arrow" />
+                <div 
+                    className={arrowClassList.join(" ")}
+                    ref={arrowContainerElement}
+                    onClick = {() => toggleContent()}
+                >
+                    <img src = {downArrow} alt="arrow" ref={arrowElement} className={props.hideDefault ? classes.rotateArrow : ""}/>
                 </div>
             </div>
             
-
-            <div className={classes.contentPart}>
+            <div 
+                style={{
+                    "max-height": maxHeight, 
+                    "padding-top": paddingForContent, 
+                    "padding-bottom": paddingForContent}} 
+                className={classes.contentPart} 
+                ref={contentPartElement}
+            >
                 {props.children}
             </div>
             
--- AI visualization/hmm-react-app/src/Components/ForwardAlgorithm/forwardAlgorithm.js
+++ AI visualization/hmm-react-app/src/Components/ForwardAlgorithm/forwardAlgorithm.js
@@ -4,18 +4,25 @@ import classes from './forwardAlgorithm.module.css';
 import Subsection from '../Common/Subsection/subsection'
 
 const ForwardAlgorithm = (props) => {
+
+    let curlyLeft = "{";
+    let curlyRight = "}";
+
+    let lambda = (x) => <span style={{"color":"#E967E0"}}>λ<sub>{x}</sub></span>;
+    let pi = (x) => <span style={{"color":"#6EC668"}}>π<sub>{x}</sub></span>;
+    let A = (x) => <span style={{"color":"#E95252"}}>A<sub>{x}</sub></span>;
+    let B = (x) => <span style={{"color":"#43AFCA"}}>B<sub>{x}</sub></span>;
+    let O = (x) => <span style={{"color":"#43AFCA"}}>O<sub>{x}</sub></span>;
+
     return (
         <div className={classes.ForwardAlgorithm}>
             <h1>Forward Algorithm</h1>
 
-            <Subsection header = "About the Forward Algorithm">
+            <Subsection header = "About the Forward Algorithm" maxHeight="140px" hideDefault={false}>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ligula elementum, malesuada magna sollicitudin, semper risus. Suspendisse vehicula, tortor non viverra placerat, arcu urna vestibulum mauris, ac tristique odio nunc in massa. In venenatis, eros sit amet fringilla euismod, sem magna efficitur sapien, eu sagittis odio erat et purus. Cras nec sapien fermentum, varius odio nec, ultricies mi. Suspendisse ut pretium nisi. Vestibulum dignissim orci eget nunc pharetra, ac ultrices risus posuere. Fusce placerat convallis congue. Integer quis efficitur enim, sit amet malesuada justo. Suspendisse sem magna, vehicula ac mattis quis, rhoncus in risus. Vivamus ac odio vitae urna congue scelerisque. Vivamus vulputate, lorem luctus vulputate dictum, enim nisl suscipit elit, eget tempus libero massa et sapien. Duis eleifend est diam, ut imperdiet tellus ullamcorper nec. Etiam venenatis orci massa, ut euismod elit pulvinar sed. Quisque vel ligula eros. Vestibulum et ultrices magna. Nullam tempor sed justo eget posuere.
-
-                    In nec nunc eget eros laoreet congue eget tristique sapien. Donec suscipit ipsum eget nunc egestas, ut egestas leo imperdiet. Nunc semper, ligula sit amet commodo egestas, tortor dolor vulputate ligula, eu semper mi ipsum ultrices justo. Nunc tincidunt sagittis dictum. Sed ut auctor nisl, at fermentum lacus. Nam vestibulum lectus ac eros sagittis, et volutpat massa tempor. Mauris in arcu justo. Phasellus in elit vitae nulla tincidunt molestie vel vel justo. Fusce eu sapien iaculis, condimentum velit at, hendrerit risus. Morbi dictum metus ligula, quis bibendum ligula pulvinar vitae. Pellentesque id magna neque. Cras vehicula lectus non nisl fringilla tempus id nec diam.
-
-                    Integer convallis lectus non sagittis facilisis. Fusce laoreet blandit suscipit. Sed accumsan finibus sapien, aliquam imperdiet dui pharetra quis. Aliquam a tortor neque. Ut accumsan posuere massa egestas rhoncus. Donec hendrerit nisi vel ante laoreet, eu tincidunt odio ornare. Mauris egestas ex ut diam sodales, et convallis sapien auctor. Vivamus ut tempus massa, ac sollicitudin leo. Duis nibh tellus, tristique sit amet mollis sit amet, tristique eu lacus. Curabitur rutrum suscipit pharetra. Nulla ultricies nisl a facilisis porta. Vestibulum venenatis metus lorem, non hendrerit felis egestas non.
-
+                    The algoritm is used to solve problems in the category <b>evaluation/filtering</b>. That is, if we want to compute the
+                    likelihood 𝑃( {O("0:T-1")} | {lambda("x")} ) = 𝑃( {O("x")} | {lambda("x")} ) = 𝑃( {curlyLeft} {O("0")}, {O("1")}, ..., {O("T-1")}
+                    {curlyRight} | {lambda("x")}). In otherwords, if we want to compute the likelihood of observing a specific sequence of observations given our {lambda("x")}  = {curlyLeft} {A("x")}, {B("x")}, {pi("x")} {curlyRight}.
                 </p>
             </Subsection>
         </div>
